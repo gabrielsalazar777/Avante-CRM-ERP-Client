9152c317455b838253b58236398091f93d96471c
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-05-24 13:16:13 -0400

    weather forecast included in projects dashboard

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 
--- src/pages/ProjectsDash.js
+++ src/pages/ProjectsDash.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProjectList from "../components/ProjectList";
 import ProjectDetails from "../components/ProjectDetails";
 import AddProject from "../components/AddProject";
+import { get } from "../services/authService";
 
 const ProjectsDash = () => {
   const [selectedProject, setSelectedProject] = useState(null);
+  const [weather, setWeather] = useState([]);
 
   const projectTypes = [
     "Re-Roof",
@@ -21,8 +23,37 @@ const ProjectsDash = () => {
     setSelectedProject(project);
   };
 
+  const getWeather = () => {
+    get("/api/weather")
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <div>
+      {weather.length ? (
+        <div>
+          {weather.map((d) => {
+            return (
+              <div>
+                <p>{d.day}</p>
+                <img src={d.icon} alt="weather-icon" />
+                <p>{d.condition}</p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
       <AddProject
         getProjectDetails={getProjectDetails}
         projectTypes={projectTypes}
